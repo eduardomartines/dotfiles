bbe2821e9c1a0650870b27c0a3bcf0b687693293
Eduardo Martines <martines.eduardo@gmail.com>
2020-10-15 22:30:04 -0300

    update install recipe

--- install.sh
+++ install.sh
@@ -1,20 +1,49 @@
 #
+# update Kernel
+cd /tmp
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900_5.9.0-050900.202010112230_all.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-image-unsigned-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-modules-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+sudo dpkg -i *.deb
+reboot
+# MANUAL STEPs: login
+
+#
+# nvidia driver
+ubuntu-drivers devices
+sudo apt install -y nvidia-driver-450
+reboot
+# MANUAL STEPs:
+#   * login
+
+#
+# basic update
+sudo sed -i '/cdrom/d' /etc/apt/sources.list
+sudo apt dist-upgrade
 sudo apt update -y
 sudo apt upgrade -y
 
+#
 # browser
-sudo apt purge -y firefox
+cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-# TODO: install LAST PASS plugin
+rm -rf google-chrome-stable_current_amd64.deb
+# MANUAL STEPs:
+# 	* install LAST PASS plugin
+# 	* sign in and sync with Google Account
+#	* go to "chrome://settings/system" and make sure the following option is disabled "Use hardware acceleration when available" to prevent system crash
+sudo apt purge -y firefox
 
 # ssh
 ssh-keygen
 cat ~/.ssh/id_rsa.pub
-# TODO: add ssh on Github and Bitbucket
+# MANUAL STEPs:
+#   * add ssh on Github (https://github.com/settings/keys) and Bitbucket
 
-# TODO: necessary?
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# some tools
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
 
 # git
 sudo apt install -y git
@@ -23,12 +52,14 @@ git config --global user.name "Eduardo Martines"
 
 # docker
 sudo snap install --classic docker
-sudo chown root:docker /var/run/docker.sock
 sudo groupadd docker
+sudo chown root:docker /var/run/docker.sock
 sudo usermod -aG docker $USER
 newgrp docker
 reboot
-# TODO: test with com "docker run hello-world"
+# MANUAL STEPs:
+#	* loing
+#	* test with com "docker run hello-world"
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -40,38 +71,32 @@ sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # i3gaps
-cd ~/
-sudo apt install -y git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev && git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps && autoreconf --force --install && rm -rf build/ && mkdir -p build && cd build/ && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers && make && sudo make install
-rm -rf i3-gaps
+sudo apt install -y git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install && rm -rf build/ && mkdir -p build && cd build/ && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers && make && sudo make install
 
 # polybar
-cd ~/
-sudo apt-get install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 && git clone https://github.com/jaagr/polybar.git && cd polybar && sudo ./build.sh
-rm -rf polybar
+sudo snap install polybar-git --edge --devmode
 
 # rofi
 sudo apt install -y rofi
 
-# TODO: necessary?
-sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
-
-# drivers
-sudo ubuntu-drivers autoinstall
+# drivers (necessary?)
+# sudo ubuntu-drivers autoinstall
 
 # terminal
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt install -y alacritty
+sudo snap install alacritty --classic
 
 # tmux
 sudo apt install -y tmux
-
-# tmuxinator
-gem install --no-document tmuxinator
 
 # visual studio Code
 sudo snap install --classic code
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
 
 # vim
 sudo apt install -y vim
@@ -80,30 +105,38 @@ sudo apt install -y vim
 sudo snap install --classic discord
 
 # dotfiles
-mkdir Projects
-cd $_
+mkdir ~/Projects
+cd ~/Projects
 git clone git@github.com:eduardomartines/dotfiles.git
 sudo apt-get install -y stow
+cd dotfiles
+# WARNING: pay all attention here
+#rm ~/.zshrc
+#rm ~/.oh-my-zsh/oh-my-zsh.sh
 ./setup.sh
-[logout/login]
+gnome-session-quit --no-prompt
+# MANUAL STEP: 
+#   * change settings to i3
+#   * login
 
 # nvm node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
-nvm install v13.3.0
-nvm alias default node
+nvm install v14.14.0
 
 # yarn
 npm install -g yarn
 
 # rbenv ruby
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-sudo apt-get install -y libssl-dev libreadline-dev
-rbenv install 2.6.5
-rbenv global 2.6.5
+sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+rbenv install 2.7.2
+rbenv global 2.7.2
+
+# tmuxinator
+gem install tmuxinator
 
 # python venv
 sudo apt install -y python3-venv
 
-# clean
+# cleanup
 sudo apt autoremove -y
