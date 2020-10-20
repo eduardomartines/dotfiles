7797ab860e6c85bb5cacbe7d52089ee4aeaf099e
Eduardo Martines <martines.eduardo@gmail.com>
2020-10-20 19:11:26 -0300

    update docker-compose

--- install.sh
+++ install.sh
@@ -73,7 +73,8 @@ su - ${USER}
 #	* test with com "docker run hello-world"
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # zsh
 sudo apt-get install -y zsh
